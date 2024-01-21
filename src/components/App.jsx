caffdc276c06414186f1936e14fcc09dc6ffee1c
Vova Vykhrystenko <mikserwowa@gmail.com>
2024-01-21 10:57:00 +0200

    Add new file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,63 +1,52 @@
-import { Component } from 'react';
-import { nanoid } from 'nanoid';
-import { InputForm } from './InputForm/InputForm';
-import { ContactsList } from './ContactsList/ContactsList';
-import { Filter } from './Filter/Filter';
-import s from './App.module.css';
+import { useState } from 'react';
+import TaskInput from './TaskInput/TaskInput';
+import TaskItem from './TaskItem/TaskItem';
+import Stats from './Stats/Stats';
 
-class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-    filter: '',
+function App(task, key) {
+  const [toDoList, setToDoList] = useState([]);
+
+  const addTask = taskName => {
+    const newTask = { taskName, checked: false };
+    setToDoList([...toDoList, newTask]);
   };
 
-  addContact = (name, number) => {
-    if (this.state.contacts.find(item => item.name === name)) {
-      alert(`${name} is already in contacts`);
-    } else {
-      this.setState(prevState => {
-        return {
-          contacts: [...prevState.contacts, { id: nanoid(), name, number }],
-        };
-      });
-    }
-  };
-
-  filterOperator = event => {
-    this.setState({ filter: event.target.value.toLowerCase() });
-  };
-
-  deleteContact = id => {
-    this.setState(prevState => {
-      return { contacts: prevState.contacts.filter(item => item.id !== id) };
-    });
-  };
-
-  render() {
-    const currentContacts = this.state.contacts.filter(item =>
-      item.name.toLowerCase().includes(this.state.filter)
-    );
-    return (
-      <div className={s.container}>
-        <h1>Phonebook</h1>
-        <InputForm addContact={this.addContact} />
-        <h2>Contacts</h2>
-        <Filter
-          filter={this.state.filter}
-          filterOperator={this.filterOperator}
-        />
-        <ContactsList
-          currentContacts={currentContacts}
-          deleteContact={this.deleteContact}
-        />
-      </div>
+  function deletTask(deletTaskName) {
+    setToDoList(toDoList.filter(task => task.taskName !== deletTaskName));
+  }
+  function toggleCheck(taskName) {
+    setToDoList(prevToDoList =>
+      prevToDoList.map(task =>
+        task.taskName === taskName ? { ...task, checked: !task.checked } : task
+      )
     );
   }
+
+  return (
+    <>
+      <div className="container">
+        <h1>Task manager</h1>
+        <TaskInput addTask={addTask} />
+        <div className="toDoList">
+          <span>To do</span>
+          <ul className="list-items">
+            {toDoList.map((task, key) => (
+              <TaskItem
+                task={task}
+                key={key}
+                deletTask={deletTask}
+                toggleCheck={toggleCheck}
+              />
+            ))}
+          </ul>
+          {toDoList.length === 0 ? (
+            <p className="notify">Your are done!</p>
+          ) : null}
+        </div>
+      </div>
+      <Stats toDoList={toDoList} />
+    </>
+  );
 }
 
 export default App;
